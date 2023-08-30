c598ee6751607b8353717d09e27cde2a85480729
TANYMART <143549005+TANYMART@users.noreply.github.com>
2023-08-30 16:40:13 -0500

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,12 @@
+# Entrada (comentario)
+nombre = "Juan Perez"  # variable que almacena una cadena o texto
+año = 1995 # variable que almacena un valor numerico
+
+#proceso
+edad = 2023 - año
+
+#Salida
+print ("Buenas noches:",nombre);
+print("Tu edad es :", edad, " años")
+print(23+8) # sumando 2 numeros y lo imprimimos
+print("23"+"8") # concatenando 2 cadenas de texto
---
+++ demo2.py
@@ -0,0 +1,11 @@
+#Entrada
+Base=12
+altura=9
+
+#Proceso
+area=Base*altura
+precio=area *750
+
+#Salida
+print("Base:","Altura:",altura,"=,",area,"m2")
+print("Precio del terreno:",area,"x 750 =",precio)
